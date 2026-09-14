f1897fd6330543b2335e7db88644bb32ccd616b8
rasikakalhari25-create <rasikakalhari25@gmail.com>
2026-09-14 10:45:24 +0900

    2nd commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  const [ notes, setNotes] = useState([]);
+  const [ selectedNote, setSelectedNote ] = useState(null);
+  const [ editedText, setEditedText ] = useState("");
+
+  const handleNoteAdd = () =>{
+
+    const newNote = {
+      id: Date.now(),
+
+      text:"新規ノート📝"
+    }
+    console.log(newNote);
+    setNotes([...notes, newNote]);
+    setSelectedNote(newNote);
+    setEditedText(newNote.text)
+  }
+  const handleSelect = (note) => {
+    console.log(note);
+    setSelectedNote(note);
+  }
+
+  const handleDelete = (noteId) =>{
+    // console.log(note.Id);
+    const filterdNote = notes.filter((note)=> note.id !== noteId);
+    console.log(filterdNote);
+    setNotes(filterdNote);
+    if(filterdNote.length > 0){
+      const lastNote = filterdNote[filterdNote.length - 1];
+      setSelectedNote(lastNote);
+    } else {
+      setSelectedNote(null);
+    }
+  }
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setEditedText(e.target.value);
+  }
+  const handleSave = () => {
+    const updatedNotes = notes.map((note)=>{
+      if(note.id == selectedNote.id){
+        return {...note, text: editedText}
+      }
+      return notes;
+    });
+    console.log(updatedNotes);
+    setNotes (updatedNotes);
+  };
+  return (
+    <div className="app-container">
+
+      <div className='sidebar'>
+        <button id="create"onClick={handleNoteAdd}>ノート追加</button>
+        <ul>
+          {notes.map((note)=>(
+          <li id={note.id}className={selectedNote.id == note.id ? "selected" : ""}>
+            <button onClick={() => handleDelete(note.id)} className='delete'>削除</button>
+            <span onClick={() => handleSelect(note)}>{note.text}</span>
+          </li>
+          ))}
+          
+        </ul>
+      </div>
+
+      <div className='main'>
+        {selectedNote ? (
+          <>
+        <h2>内容</h2>
+        <textarea value={editedText}onChange={handleChange}/>
+        <button onClick={handleSave}className='save'>保存</button>
+        </>
+        ) : (
+          <div>
+            ノートを作成してください。
+          </div>
+        )}
+        </div>
+    </div>
+  );
+
+}
+export default App;
